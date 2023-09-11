3d62004f7f9591bee8dfe1107b2349f35186b10c
Christian-Beninati <christianbeninati.info@gmail.com>
2023-09-11 20:00:41 +0100

    Added Header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script >
-
+import AppHeader from './components/AppHeader.vue';
 export default {
-
+  components: { AppHeader }
 }
 </script>
 
 <template>
+  <AppHeader />
   <main class="container my-3">
     <h1>Ciao</h1>
   </main>

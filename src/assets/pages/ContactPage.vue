ed5f4b3e35172ae45438f82755044f6e504444df
Christian-Beninati <christianbeninati.info@gmail.com>
2023-09-13 19:31:40 +0100

    Added Validation Front-End

--- src/assets/pages/ContactPage.vue
+++ src/assets/pages/ContactPage.vue
@@ -5,14 +5,62 @@ const emptyForm = { email: "", subject: "", content: "", subscription: false, };
 export default {
     data: () => ({
         form: emptyForm,
+        errors: {},
+        successMessage: null,
+
     }),
+    computed: {
+        hasErrors() {
+            return Object.keys(this.errors).length;
+        },
+        showAlert() {
+            return Boolean(this.hasErrors || this.successMessage);
+        },
+        alertType() {
+            return this.hasErrors ? "danger" : "success"
+        }
+    },
     methods: {
         sendForm() {
+            this.successMessage = null;
+            this.validateForm();
+            if (this.hasErrors) return;
             axios.post(endpoint, this.form)
-                .then(() => { this.form = emptyForm })
-                .catch((err) => { console.error(err) })
+                .then(() => {
+                    this.form = { emptyForm };
+                    this.successMessage = "Message sent successfully!";
+                })
+                .catch((err) => {
+                    if (err.response.status === 400) {
+                        const errors = err.response.data.errors;
+                        const errorMessages = {};
+                        for (let key in errors) errorMessages[key] = errors[key][0];
+                        this.errors = errorMessages;
+                    } else {
+                        console.error(err);
+                        this.errors = { network: "An error has occurred!" }
+                    }
+                })
                 .then(() => { })
+        },
+        validateForm() {
+            this.errors = {};
+            // Valido email
+            if (!this.form.email) {
+                this.errors.email = "E-mail is required";
+            } else if (!this.form.email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+                this.errors.email = "The email does not have a valid format";
+            }
+            // Valido il subject
+            if (!this.form.subject) {
+                this.errors.subject = "The email must contain the subject line";
+            }
+            // Valido il content
+            if (!this.form.content) {
+                this.errors.content = "The e-mail must contain a content";
+            }
         }
+
     }
 };
 </script>
@@ -20,25 +68,45 @@ export default {
 <template>
     <div class="container">
         <h2 class="my-4">Contact us</h2>
-        <!-- Form Contatti -->
-        <form @submit.prevent="sendForm">
+
+        <!-- Alert -->
+        <div v-if="showAlert" class="alert" :class="`alert-${alertType}`" role="alert">
+            <div v-if="successMessage">{{ successMessage }}</div>
+            <ul v-if="hasErrors">
+                <li v-for="(error, key) in errors" :key="key">{{ error }}</li>
+            </ul>
+        </div>
+
+        <!-- Form Contacts -->
+        <form @submit.prevent="sendForm" novalidate>
             <!-- Email -->
             <div class="mb-3">
                 <label for="email" class="form-label">Your email<sup class="text-danger">*</sup></label>
-                <input type="email" class="form-control" id="email" placeholder="example@hello.com"
-                    v-model.trim="form.email">
+                <input type="email" class="form-control" :class="{ 'is-invalid': errors.email }" id="email"
+                    placeholder="example@hello.com" v-model.trim="form.email">
+                <div class="invalid-feedback">
+                    {{ errors.email }}
+                </div>
             </div>
 
-            <!-- Oggetto -->
+            <!-- Object -->
             <div class="mb-3">
-                <label for="subject" class="form-label">Subject of the email<sup class="text-danger">*</sup></label>
-                <input type="text" class="form-control" id="subject" v-model.trim="form.subject">
+                <label for="subject" class="form-label">Subject<sup class="text-danger">*</sup></label>
+                <input type="text" :class="{ 'is-invalid': errors.subject }" class="form-control" id="subject"
+                    v-model.trim="form.subject">
+                <div class="invalid-feedback">
+                    {{ errors.subject }}
+                </div>
             </div>
 
-            <!-- Messaggio -->
+            <!-- Message -->
             <div class="mb-3">
-                <label for="message" class="form-label">Content of the email<sup class="text-danger">*</sup></label>
-                <textarea class="form-control" id="message" rows="5" v-model.trim="form.content"></textarea>
+                <label for="content" class="form-label">Content<sup class="text-danger">*</sup></label>
+                <textarea class="form-control" :class="{ 'is-invalid': errors.content }" id="content" rows="5"
+                    v-model.trim="form.content"></textarea>
+                <div class="invalid-feedback">
+                    {{ errors.content }}
+                </div>
             </div>
 
             <div class="form-check">
